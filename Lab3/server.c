eeabb309ac9c002719b0cb71ffcf7e6407c1d5ed
taylorjflatt@gmail.com <taylorjflatt@gmail.com>
2017-10-15 20:39:22 -0500

    Finished standardizing server's documentation.

--- Lab3/server.c
+++ Lab3/server.c
@@ -196,14 +196,12 @@ void *epoll_listener(void * ignore) {
                 DTRACE("%ld:Starting data transfer PTY-->socket (FD %d-->%d)\n",(long)getpid(),ev_list[i].data.fd, client_fd_tuples[ev_list[i].data.fd]);
                 if(transfer_data(ev_list[i].data.fd, client_fd_tuples[ev_list[i].data.fd])) {
                     perror("Error reading/writing to the client. Closing shop.\n");
-                    //kill(bash_fd[ev_list[i].data.fd], SIGTERM);
                     close(ev_list[i].data.fd);
                     close(client_fd_tuples[ev_list[i].data.fd]);
                 }
                 DTRACE("%ld:Completed data transfer PTY-->socket\n",(long)getpid());
             } else if(ev_list[i].events & (EPOLLHUP | EPOLLRDHUP | EPOLLERR)) {
                 fprintf(stderr, "Received an EPOLLHUP or EPOLLERR on %d. Shutting it and %d down.\n", ev_list[i].data.fd, client_fd_tuples[ev_list[i].data.fd]);
-                //kill(bash_fd[ev_list[i].data.fd], SIGTERM);
                 close(client_fd_tuples[ev_list[i].data.fd]);
             }
         }
@@ -220,12 +218,11 @@ void *epoll_listener(void * ignore) {
 */
 void  *handle_client(void *client_fd_ptr) {
 
-    // Dereference the int pointer and get rid of the memory now that we no longer need it.
+    /* Dereference the int pointer and get rid of the memory now that we no longer need it. */
     int client_fd = *(int *) client_fd_ptr;
     free(client_fd_ptr);
     pthread_detach(pthread_self());
 
-    // Conduct the three-way handshake with the client.
     if(handshake(client_fd) == -1) {
         perror("Client failed the handshake.");
         close(client_fd);
@@ -255,16 +252,15 @@ int handshake(int client_fd) {
     
     DTRACE("%ld:Starting handshake with CLIENT=%d.\n",(long)getppid(), client_fd);   
 
-    // Three second timer.
+    /* Three second timer. */
     static struct itimerspec timer;
     timer.it_value.tv_sec = 3;
 
-    // Create the signal action.
+    /* Create the signal handler and event for the timer. */
     struct sigaction sig_act;
     sig_act.sa_flags = SA_SIGINFO;
     sig_act.sa_sigaction = &sighandshake_handler;
 
-    // Create a signal event tied to a specific thread.
     struct sigevent sig_ev;
     sig_ev.sigev_signo = SIGALRM;
     sig_ev.sigev_notify = SIGEV_THREAD_ID;
@@ -279,8 +275,7 @@ int handshake(int client_fd) {
         perror("Error setting up sigaction.");
     }
 
-    // Setup the signal event with the appropriate flags and assign to a 
-    // specific thread.
+    /* Set the timer to the specific thread. */
     sig_ev.sigev_value.sival_ptr = &alarm;
     sig_ev._sigev_un._tid = syscall(SYS_gettid);
 
@@ -342,29 +337,35 @@ int pty_open(int client_fd) {
 
     DTRACE("%ld:Opening PTY with CLIENT=%d.\n",(long)getppid(), client_fd);  
 
-    /* Open an unused pty dev and store the fd for later reference.
-        O_RDWR := Open pty for reading + writing. 
-        O_NOCTTY := Don't make it a controlling terminal for the process. 
+    /** Open an unused pty dev and store the fd for later reference.
+     * 
+     * O_RDWR = Open pty for read/write.
+     * O_NOCTTY = Don't make it a controlling terminal for the process.
+     * O_CLOEXEC = Close the fd on exec.
     */
-    if((pty_master = posix_openpt(O_RDWR|O_NOCTTY|O_CLOEXEC)) == -1) {
+    if((pty_master = posix_openpt(O_RDWR | O_NOCTTY | O_CLOEXEC)) == -1) {
         perror("Failed openpt.");
         return -1;        
     }
 
-    /// Set pty master fd so it gets closed when bash execs.
-    fcntl(pty_master,F_SETFD,FD_CLOEXEC);
+    /* Set pty_master to get closed when bash execs. */
+    fcntl(pty_master, F_SETFD, FD_CLOEXEC);
 
     if(set_nonblocking_fd(pty_master) == -1) {
         perror("Error setting client to non-blocking.");
     }
     
-    /* Attempt to kickstart the master.
-        Grantpt := Creates a child process that executes a set-user-ID-root program changing ownership of the slave 
-            to be the same as the effective user ID of the calling process and changes permissions so the owner 
-            has R/W permissions.
-        Unlockpt := Removes the internal lock placed on the slave corresponding to the pty. (This must be after grantpt).
-        ptsname := Returns the name of the pty slave corresponding to the pty master referred to by pty_master. (/dev/pts/nn).
-    */
+    /** Attempt to kickstart the master.
+     * 
+     * Grantpt = Creates a child process that executes a set-user-ID-root
+     *          program changing ownership of the slave to be the same as  
+     *          the effective user ID of the calling process and changes 
+     *          permissions so the owner has R/W permissions.
+     * Unlockpt = Removes the internal lock placed on the slave corresponding 
+     *          to the pty. (This must be after grantpt).
+     * ptsname = Returns the name of the pty slave corresponding to the pty 
+     *          master referred to by pty_master. (/dev/pts/nn).
+     */
     if(grantpt(pty_master) == -1 || unlockpt(pty_master) == -1 || (pty_slave = ptsname(pty_master)) == NULL) {
         err = errno;
         close(pty_master);
@@ -375,20 +376,18 @@ int pty_open(int client_fd) {
     pty_slave = (char *) malloc(1024);
     strcpy(pty_slave, ptsname(pty_master));
 
-    /// Create the bash process.
-    ///
-    /// Fork off a new bash process and redirect stdin/stdout/stderr appropriately.
+    /* Create bash subprocess. */
     if((b_pid = fork()) == 0) {
         DTRACE("%ld:PTY_MASTER=%i and PTY_SLAVE=%s.\n",(long)getppid(), pty_master, pty_slave);  
         
-        // pty_master is not needed in the child, close it.
-        close(pty_master);
-        close(client_fd);
+        close(pty_master);  /* No longer needed. Close it. */
+        close(client_fd);   /* No longer needed. Close it. */
+
         if(create_bash_process(pty_slave) == -1) {
             perror("Failed to create bash process.");
         }
 
-        // Should never reach this point since the bash process will just terminate.
+        /* It should never reach this point. */
         exit(EXIT_FAILURE);
     }
 
@@ -401,7 +400,7 @@ int pty_open(int client_fd) {
     bash_fd[client_fd] = b_pid;
     bash_fd[pty_master] = b_pid;
 
-    // Setup the epoll event handling (R/W).
+    /* Setup the epoll event handling (R/W). */
     ep_ev[0].data.fd = client_fd;
     ep_ev[0].events = EPOLLIN | EPOLLET;
 
@@ -436,8 +435,13 @@ int create_bash_process(char *pty_slave) {
         return -1;            
     }
 
-    // Setup the pty for the bash subprocess.
-    if((pty_slave_fd = open(pty_slave, O_RDWR|O_NOCTTY|O_CLOEXEC)) == -1) {
+    /** Setup the pty for the bash subprocess.
+     * 
+     * O_RDWR = Open pty for read/write.
+     * O_NOCTTY = Don't make it a controlling terminal for the process.
+     * O_CLOEXEC = Close the fd on exec.
+    */
+    if((pty_slave_fd = open(pty_slave, O_RDWR | O_NOCTTY | O_CLOEXEC)) == -1) {
         perror("Failed opening PTY_SLAVE.");
         return -1;
     }
@@ -462,16 +466,16 @@ int set_nonblocking_fd(int fd) {
     
     int fd_flags;
 
-    // Get the current fd flags.
+    /* Get the current fd's flags. */
     if((fd_flags = fcntl(fd, F_GETFL, 0)) == -1) {
         perror("Error getting fd_flags.");
         return -1;
     }
 
-    // Add the non-blocking flag.
+    /* Append the non-blocking flag. */
     fd_flags |= O_NONBLOCK;
 
-    // Set the new flag set for the fd.
+    /* Overwrite the fd's flags. */
     if(fcntl(fd, F_SETFL, fd_flags) == -1) {
         perror("Error setting fd_flags.");
         return -1;
