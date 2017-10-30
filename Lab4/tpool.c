5855fc5b31d09ca9125e680359a618b29cba1e07
taylorjflatt@gmail.com <taylorjflatt@gmail.com>
2017-10-29 21:24:44 -0500

    Added push and pop methods.

--- Lab4/tpool.c
+++ Lab4/tpool.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -21,7 +20,7 @@ typedef struct thread {
 } thread;
 
 typedef struct task_queue {
-    pthread_mutex_t mutex;    /* R/W Lock */
+    pthread_mutex_t mutex;    /* R/W mutex */
     semaphore* has_jobs;      /* Contains any tasks. */
     size_t len;               /* Number of jobs in queue. */
     int head;                 /* Front of the queue. */
@@ -42,8 +41,17 @@ static task_queue queue;
 
 /* Prototypes. */
 
-int tpool_add_task(int task)
-static int queue_init(int len);
+//int tpool_add_task(int task)
+//static int queue_init(int len);
+
+static void* thread_loop(void* thread);
+static int thread_init(thread** threadpp, int ord);
+
+static int queue_init();
+static int push(task_queue* q, int task);
+static int pop(task_queue* q);
+static int resize_queue(task_queue* q);
+static void pool_wait(semaphore* sem);
 
 int tpool_init(void (*task)(int)) {
 
@@ -91,9 +99,9 @@ int tpool_init(void (*task)(int)) {
     }
 
     /* Create memory for the semaphore. */
-    queue.has_task = (sem*) malloc(sizeof(sem));
-    if (queue.has_task == NULL) {
-        perror("queue_init(): Failed to allocate memory for sem");
+    queue.has_jobs = (semaphore*) malloc(sizeof(semaphore));
+    if (queue.has_jobs == NULL) {
+        perror("queue_init(): Failed to allocate memory for semaphore");
         return -1;
     }
 
@@ -103,10 +111,10 @@ int tpool_init(void (*task)(int)) {
     queue.tail = 0;
 
     /* Initialize the locks and set waiting. */
-    pthread_mutex_init(&queue.lock, NULL);
-    pthread_mutex_init(&(queue.has_task->mutex), NULL);
-    pthread_cond_init(&(queue.has_task->condition), NULL);
-    sem->flag = 0;
+    pthread_mutex_init(&queue.mutex, NULL);
+    pthread_mutex_init(&(queue.has_jobs->mutex), NULL);
+    pthread_cond_init(&(queue.has_jobs->condition), NULL);
+    queue.has_jobs -> flag = 0;
 
     return 0;
 }
@@ -128,3 +136,100 @@ static int thread_init(thread** threadptr, int ord) {
 
     return 0;
   }
+
+static void* thread_loop(void *thr) {
+
+    int task;
+
+    /* If the queue is empty, wait. Then run any new jobs. */
+    for(;;) {
+        pool_wait(tpool.queue -> has_jobs);
+        pthread_mutex_lock(&tpool.queue -> mutex);
+        task = pop(tpool.queue);
+        pthread_mutex_unlock(&tpool.queue -> mutex);
+
+        tpool.subroutine(task);
+    }
+
+    return NULL;
+}
+
+static void pool_wait(semaphore* sem) {
+
+    pthread_mutex_lock(&sem -> mutex);
+
+    while(sem -> flag == 0) {
+        pthread_cond_wait(&sem -> condition, &sem -> mutex);
+    }
+
+    sem -> flag -= 1;
+    pthread_mutex_unlock(&sem -> mutex);
+}
+
+int tpool_add_task(int task) {
+    int rval;
+
+    /* Add a task to the pool. */
+    pthread_mutex_lock(&tpool.queue -> mutex);
+    rval = push(tpool.queue, task);
+    pthread_mutex_unlock(&tpool.queue -> mutex);
+
+    return rval;
+}
+
+static int push(task_queue *q, int task) {
+
+    /* Check if the queue is full. */
+    if(((q->tail + 1) % (int)q->len) == q->head) {
+        if(resize_queue(q)) {
+            perror("(push) resize_queue(): Failed to increase the size of the queue.");
+            return -1;
+        }
+    }
+
+    /* Add the task to the end of the queue. */
+    q -> buffer[q -> tail] = task;
+
+    /* Move the tail down. */
+    q -> tail = (q -> tail + 1) % q -> len;
+
+    /* Signal that there is a new task */
+    pthread_mutex_lock(&q->has_jobs -> mutex);
+    queue.has_jobs -> flag += 1;
+    pthread_cond_signal(&q->has_jobs -> condition);
+    pthread_mutex_unlock(&q->has_jobs -> mutex);
+
+    return 0;
+}
+
+static int resize_queue(task_queue *q) {
+    static int i;
+    size_t newLen = q -> len * 2;
+    q -> buffer = (int *) realloc(q -> buffer, newLen * sizeof(int));
+
+    if(q -> buffer == NULL) {
+        perror("(resize_queue): Failed to realloc memory for the new buffer.");
+        return -1;
+    }
+
+    /* The head is past the tail */
+    if(q -> head > q -> tail) {
+        /* Move the head portion in front of the queue */
+        for(i = q-> head; i < (int) q -> len; i++) {
+            q -> buffer[q -> len + 1] = q -> buffer[i];
+        }
+        q -> head += q -> len;
+    }
+    q -> len = newLen;
+
+    return 0;
+}
+
+static int pop(task_queue *q) {
+
+    int task = q -> buffer[q -> head];
+    q -> head = (q -> head + 1) % q -> len;
+
+    return task;
+}
+
