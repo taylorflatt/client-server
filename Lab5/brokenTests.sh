9b551e739275cbecb0440996484163ffcd89bc28
taylorjflatt@gmail.com <taylorjflatt@gmail.com>
2017-11-23 18:05:00 -0600

    Updated testing script.

--- Lab5/brokenTests.sh
+++ Lab5/brokenTests.sh
@@ -1,62 +1,66 @@
 #!/bin/bash
 
-# Start the server.
+# Make and start the server.
+cd Lab5 && make serverD
 ./server &
 
 ip="127.0.0.1"
 errors=0
 
+# Make the clients.
+cd BrokenClients && make allclientsD 
+
 # Run the tests.
-if BrokenClients/client-wait-on-handshake $ip; then
+if ./client-wait-on-handshake $ip; then
     echo "Passed wait on handshake test!"
 else
     echo "Failed wait on handshake test!"
     ((errors++))
 fi
 
-if BrokenClients/client-wait-on-connect $ip; then
+if ./client-wait-on-connect $ip; then
     echo "Passed wait on connect test!"
 else
     echo "Failed wait on connect test!"
     ((errors++))
 fi
 
-if BrokenClients/client-parent-sigquit $ip; then
+if ./client-parent-sigquit $ip; then
     echo "Passed parent sigquit test!"
 else
     echo "Failed parent sigquit test!!"
     ((errors++))
 fi
 
-if BrokenClients/client-parent-sigint $ip; then
+if ./client-parent-sigint $ip; then
     echo "Passed parent sigint test!"
 else
     echo "Failed parent sigint test!!"
     ((errors++))
 fi
 
-if BrokenClients/client-failed-secret $ip; then
+if ./client-failed-secret $ip; then
     echo "Passed client failed secret!"
 else
     echo "Failed client failed secret!"
     ((errors++))
 fi
 
-if BrokenClients/client-failed-proceed $ip; then
+if ./client-failed-proceed $ip; then
     echo "Passed client failed proceed!"
 else
     echo "Failed client failed proceed!"
     ((errors++))
 fi
 
-if BrokenClients/client-failed-port $ip; then
+if ./client-failed-port $ip; then
     echo "Passed client failed port!"
 else
     echo "Failed client failed port!"
     ((errors++))
 fi
 
-if BrokenClients/client-failed-challenge $ip; then
+if ./client-failed-challenge $ip; then
     echo "Passed client failed challenge!"
 else
     echo "Failed client failed challenge!"
